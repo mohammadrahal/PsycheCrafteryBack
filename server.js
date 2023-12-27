950d1a0bee71241d79c6dce67541b825ea4ba190
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-12-27 18:04:05 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express();
+
+const PORT = process.env.PORT
+const cors =require('cors')
+const bodyParser = require('body-parser')
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+
+
+app.get('/', (_, res) => {
+    res.send(`Hello World!`)
+})
+
+
+app.listen(PORT , function(error){
+    if (error) console.log(`error in setup`)
+    console.log(`server is running on http://localhost:${PORT}`)
+})
